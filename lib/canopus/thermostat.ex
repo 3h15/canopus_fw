d78305d0f0ca430a8541b95e6a3350bcdfededd2
Cyprien Poisson <c.poisson@3h15.com>
2019-04-03 18:58:26 +0200

    Add n_sim_open param
    Allows to dynamically change the number of rooms whose valves are open.

--- lib/canopus/thermostat.ex
+++ lib/canopus/thermostat.ex
@@ -1,7 +1,7 @@
 defmodule Canopus.Thermostat do
 
   @persist "/root/rooms.json"
-  
+
   use GenServer
   require Logger
 
@@ -9,7 +9,7 @@ defmodule Canopus.Thermostat do
   def start_link(_) do
     GenServer.start_link(__MODULE__, %{}, name: __MODULE__)
   end
-  
+
   def init(_) do
     Logger.metadata(service: :thermostat)
     # Room structure
@@ -23,11 +23,17 @@ defmodule Canopus.Thermostat do
       hit_count: 0,
     }
     # List of rooms
-    state = %{a: room, b: room, c: room, d: room, e: room, f: room, g: room, h: room, i: room, j: room, k: room}
+    rooms = %{a: room, b: room, c: room, d: room, e: room, f: room, g: room, h: room, i: room, j: room, k: room}
+
+    # State
+    state = %{
+      rooms: rooms,
+      n_sim_open: 2
+    }
 
     # Update with data from disk
     state = read_data_from_disk(state)
-    
+
     {:ok, state}
   end
 
@@ -36,25 +42,40 @@ defmodule Canopus.Thermostat do
     sensor_id = sensor_id |> String.downcase |> String.to_existing_atom
 
     # Get room's map and update it
-    room = state[sensor_id]
+    room = state.rooms[sensor_id]
     room = %{room | target_temperature: target_temperature}
 
     # Put map back into state
-    state = state |> Map.put(sensor_id, room)
+    state = put_in state.rooms[sensor_id], room
 
     state = state |> adjust_heating
 
     # Persist state
     persist( state )
 
-    {:reply, state[sensor_id], state}
+    {:reply, state.rooms[sensor_id], state}
+  end
+
+  def handle_call( {:set_n_sim_open, value}, _from, state ) do
+
+    value = case value do
+      x when x < 2 -> 2
+      x -> x
+    end
+    state = %{state | n_sim_open: value}
+    state = state |> adjust_heating
+
+    # Persist state
+    persist( state )
+
+    {:reply, value, state}
   end
 
   def handle_call( {:update_sensor, sensor_id, temperature}, _from, state ) do
     # Get atom from sensor id binary
     sensor_id = sensor_id |> String.downcase |> String.to_existing_atom
-    # Get correponding room map
-    room = state[sensor_id]
+    # Get corresponding room map
+    room = state.rooms[sensor_id]
 
     # Compute timings and hit count
     now = Canopus.Clock.clock_time
@@ -82,7 +103,7 @@ defmodule Canopus.Thermostat do
     room = %{room | temperature: temperature}
 
     # Put map back into state
-    state = state |> Map.put(sensor_id, room)
+    state = put_in state.rooms[sensor_id], room
 
     state = state |> adjust_heating
 
@@ -93,55 +114,61 @@ defmodule Canopus.Thermostat do
   end
 
   def handle_call( :get_all, _from, state ) do
-    {:reply, state, state}
+    {:reply, state.rooms, state}
   end
 
   def handle_call( {:get, sensor_id}, _from, state ) do
-    {:reply, state[sensor_id], state}
+    {:reply, state.rooms[sensor_id], state}
   end
 
   def handle_call( :get_average_temperature, _from, state ) do
-    # Get a list of rooms 
-    rooms = state |> Map.values
+    # Get a list of rooms
+    rooms = state.rooms |> Map.values
 
     # Compute averages
     avg_temperature = Enum.reduce(rooms, 0, fn(r, s) -> s + r.temperature end) / Enum.count(rooms)
-    
+
     {:reply, avg_temperature, state}
+  end
+
+  def handle_call( :get_n_sim_open, _from, state ) do
+    {:reply, state.n_sim_open, state}
   end
 
   defp adjust_heating( state ) do
     # Reset all rooms
-    state = state |> Enum.map(fn({k, v})->
+    rooms = state.rooms |> Enum.map(fn({k, v})->
       {k, Map.put(v, :heating_is_on, false)}
     end)
     |> Map.new
-    
+
+    n_sim_open = state.n_sim_open - 1
+
     # Find the rooms that needs heating
     # We sort them by their difference to target temp
     # And keep the 2 first rooms
     # This ensure we are always heating 2 rooms which protects pumps
-    [room_1_id, room_2_id] = state
+    ids_to_open = rooms
     |> Enum.sort_by(fn({_k, room})->
       room.temperature - room.target_temperature
     end)
-    |> Enum.slice(0..1)
+    |> Enum.slice(0..n_sim_open)
     |> Enum.map(fn({k, _v})->k end)
 
-    # Set heeating flag in rooms
-    room_1 = state[room_1_id] |> Map.put(:heating_is_on, true)
-    room_2 = state[room_2_id] |> Map.put(:heating_is_on, true)
-    
-    # Put rooms back into state
-    state = state |> Map.put(room_1_id, room_1)
-    state = state |> Map.put(room_2_id, room_2)
+    rooms = ids_to_open
+    |> Enum.reduce( rooms, fn(id_to_open, rooms)->
+      # Set heating flag in room
+      room = rooms[id_to_open] |> Map.put(:heating_is_on, true)
+      # Put rooms back into state
+      rooms |> Map.put(id_to_open, room)
+    end)
 
     # Open and close valves
-    Enum.each(state, fn({room_id, room})->
+    Enum.each(rooms, fn({room_id, room})->
       valves = Canopus.HW.get_by_sensor_id(room_id).valves
       valveState = case room.heating_is_on do
         # :off valve closed, :on valve open
-        # In case of error, keep valve opened for security  
+        # In case of error, keep valve opened for security
          false -> :off
          _ -> :on
       end
@@ -149,8 +176,8 @@ defmodule Canopus.Thermostat do
         GenServer.call(Canopus.I2c, %{pin: valve_pin, to: valveState})
       end)
     end)
-    
-    state
+
+    %{state | rooms: rooms}
   end
 
 
@@ -171,13 +198,23 @@ defmodule Canopus.Thermostat do
     # We copy values one by one, using state structure to get the values
     # This way, we avoid any json corruption to survive a reboot
     # and removed properties won't stay in file forever.
-    state
-    |> Enum.map( fn( {id, room} ) ->
+    rooms = case persisted_state |> Map.has_key?("rooms") do
+      false -> state.rooms
+      true  -> state.rooms |> read_persisted_rooms(persisted_state["rooms"])
+    end
+    n_sim_open = case persisted_state |> Map.has_key?("n_sim_open") do
+      false -> state.n_sim_open
+      true  -> persisted_state["n_sim_open"]
+    end
+    %{state | rooms: rooms, n_sim_open: n_sim_open}
+  end
+
+  defp read_persisted_rooms rooms, persisted_rooms do
+    rooms |> Enum.map( fn( {id, room} ) ->
       {
         id,
-        room
-        |> Enum.map( fn( {key, value} ) ->
-          persisted_value = persisted_state[to_string(id)][to_string(key)]
+        room |> Enum.map( fn( {key, value} ) ->
+          persisted_value = persisted_rooms[to_string(id)][to_string(key)]
           {key, persisted_value || value}
         end)
         |> Map.new
@@ -191,5 +228,5 @@ defmodule Canopus.Thermostat do
     File.write!(@persist, state)
     Logger.info('Saved state.')
   end
-  
+
 end
